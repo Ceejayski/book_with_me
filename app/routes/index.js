d5ca50538b58bb7a9509f60902a3a726eca60698
Filip Jerga <filipjerga@Filips-MacBook-Pro.local>
2018-03-15 17:27:06 +0100

    Create rental model

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,17 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+
+  model(params) {
+    return this.get('store').createRecord('rental', {
+       title: 'Some nice apartment',
+       city: 'New York',
+       street: 'Time Square',
+       category: 'Apartment',
+       image: 'http://via.placeholder.com/350x250',
+       bedrooms: 5,
+       description: 'Very nice cozy apartment',
+       daily_rate: 20,
+       created_at: '20/4/2018' })
+  }
 });
