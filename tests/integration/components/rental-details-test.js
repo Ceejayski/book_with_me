07306f25844ab19f977b863d83eab56d69d0cd66
Filip Jerga <filipjerga@Filips-MacBook-Pro.local>
2018-03-19 21:18:20 +0100

    Adds test for components of rental show template

--- tests/integration/components/rental-details-test.js
+++ tests/integration/components/rental-details-test.js
@@ -6,19 +6,27 @@ moduleForComponent('rental-details', 'Integration | Component | rental details',
 });
 
 test('it renders', function(assert) {
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
+  const rental = {
+    title: 'test title',
+    city: 'test city',
+    street: 'test street',
+    category: 'test category',
+    shared: true,
+    image: 'test',
+    bedrooms: 2,
+    description: 'test description',
+    daily_rate: 11,
+    created_at: '27/12/1990'
+  };
 
-  this.render(hbs`{{rental-details}}`);
+  this.set('rental', rental);
 
-  assert.equal(this.$().text().trim(), '');
-
-  // Template block usage:
   this.render(hbs`
-    {{#rental-details}}
-      template block text
+    {{#rental-details rental=rental}}
     {{/rental-details}}
   `);
 
-  assert.equal(this.$().text().trim(), 'template block text');
+  assert.equal(this.$('.rental-title').text(), 'test title');
+  assert.equal(this.$('.rental-city').text(), 'test city');
+  assert.equal(this.$('.rental-description').text(), 'test description');
 });
